20f48518921b6404a1e70e24ba0e5c68939cab42
andatoshiki <101481353+andatoshiki@users.noreply.github.com>
2023-01-18 14:10:40 +0800

    refactor: remove unnecessary infos on bots load in terminal

--- bots/bots.go
+++ bots/bots.go
@@ -20,9 +20,8 @@ const (
 	/_  __/__  ___ / /  (_) /__ (_) / __/ __/
 	 / / / _ \(_-</ _ \/ /  '_// / / _//__ \ 
 	/_/  \___/___/_//_/_/_/\_\/_/ /___/____/ 		
-	@andatoshiki | 安田俊樹 | www.toshiki.top
-	@chenskiro | chenskiro | www.chenskiro.com
-		❤ 私はいつもChenskiroを愛します~				 
+	@andatoshiki | 安田俊樹 | www.toshiki.dev
+	@chenskiro | chenskiro | www.chenskiro.com			 
 `
 )
 
